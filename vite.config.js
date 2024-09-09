2ebf05e7f8ca24ef9c65700fe10adaa01558fe22
OndrejVojik <vojik.cz@gmail.com>
2024-09-09 18:51:03 +0200

    initial commint

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig, loadEnv } from 'vite';
+
+export default defineConfig(({ command, mode }) => {
+  // Load environment variables based on the current mode (development or production)
+  const env = loadEnv(mode, process.cwd(), '');
+
+  // Use the loaded environment variable to set the base path
+  const basePath = env.VITE_BASE_PATH || '/';
+
+  return {
+    plugins: [],
+    base: basePath,
+  };
+});
