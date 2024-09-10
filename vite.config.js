9e8f66adb9c51af63a3cffc160beb48b827c5faf
OndrejVojik <vojik.cz@gmail.com>
2024-09-10 13:34:21 +0200

    15

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   // Use the loaded environment variable to set the base path
-  const basePath = VITE_BASE_PATH || '/';
+  const basePath = env.VITE_BASE_PATH || '/';
   console.log('basePath', basePath);
 
   return {
