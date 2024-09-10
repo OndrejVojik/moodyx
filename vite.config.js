d86880499006ed90f4c38c0727db84c6dc07ede3
OndrejVojik <vojik.cz@gmail.com>
2024-09-10 13:31:54 +0200

    14

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ export default defineConfig(({ command, mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
 
   // Use the loaded environment variable to set the base path
-  const basePath = env.VITE_BASE_PATH || '/';
+  const basePath = VITE_BASE_PATH || '/';
+  console.log('basePath', basePath);
 
   return {
     plugins: [],
-	// base: '/moodyx/',
+	base: basePath,
   };
 });
